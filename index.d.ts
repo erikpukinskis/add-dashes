f24e5125a24222b44a4d4b00fef597d2a77767ad
Erik Pukinskis <epukinskis@outlinerisk.com>
2019-10-22 00:30:31 -0700

    TypeScript support

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+declare module 'add-dashes' {
+    /**
+     * Converts any string, in a variety of formats to
+     * a-nice-kebab-case
+     * @param {string} str A string like "camelCase", "under_score_case", "just some text"
+     */
+    export default function addDashes(str: string): string
+}
